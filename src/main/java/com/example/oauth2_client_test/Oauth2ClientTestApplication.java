0bfbf2baec39269facd12e35fef74d84e79e0075
maylie-vu <maylie.anhvu@gmail.com>
2025-07-29 10:04:17 -0400

    added source files

---
+++ src/main/java/com/example/oauth2_client_test/Oauth2ClientTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_client_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientTestApplication.class, args);
+	}
+
+}
